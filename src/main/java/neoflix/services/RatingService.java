bd71a1918cc98e5ae4806b4f826741d0b0c19260
Luis Fernando Ushiña <guffenix@gmail.com>
2022-06-05 23:56:12 -0500

    implementation to add rating service with handle error, use case can be test with 'mvn test -Dtest=neoflix._06_RatingMoviesTest#writeMovieRatingAsInt'

--- src/main/java/neoflix/services/RatingService.java
+++ src/main/java/neoflix/services/RatingService.java
@@ -2,7 +2,11 @@ package neoflix.services;
 
 import neoflix.AppUtils;
 import neoflix.Params;
+import neoflix.ValidationException;
+
 import org.neo4j.driver.Driver;
+import org.neo4j.driver.Values;
+import org.neo4j.driver.exceptions.NoSuchRecordException;
 
 import java.util.HashMap;
 import java.util.List;
@@ -26,26 +30,26 @@ public class RatingService {
     /**
      * Return a paginated list of reviews for a Movie.
      *
-     * Results should be ordered by the `sort` parameter, and in the direction specified
+     * Results should be ordered by the `sort` parameter, and in the direction
+     * specified
      * in the `order` parameter.
      * Results should be limited to the number passed as `limit`.
      * The `skip` variable should be used to skip a certain number of rows.
      *
-     * @param {string} id       The tmdbId for the movie
-     * @param {string} sort  The field to order the results by
-     * @param {string} order    The direction of the order (ASC/DESC)
-     * @param {number} limit    The total number of records to return
-     * @param {number} skip     The number of records to skip
+     * @param {string} id The tmdbId for the movie
+     * @param {string} sort The field to order the results by
+     * @param {string} order The direction of the order (ASC/DESC)
+     * @param {number} limit The total number of records to return
+     * @param {number} skip The number of records to skip
      * @returns {Promise<Record<string, any>>}
      */
     // tag::forMovie[]
-    public List<Map<String,Object>> forMovie(String id, Params params) {
+    public List<Map<String, Object>> forMovie(String id, Params params) {
         // TODO: Get ratings for a Movie
 
-        return AppUtils.process(ratings,params);
+        return AppUtils.process(ratings, params);
     }
     // end::forMovie[]
-
 
     /**
      * Add a relationship between a User and Movie with a `rating` property.
@@ -53,20 +57,46 @@ public class RatingService {
      *
      * If the User or Movie cannot be found, a NotFoundError should be thrown
      *
-     * @param {string} userId   the userId for the user
-     * @param {string} movieId  The tmdbId for the Movie
-     * @param {number} rating   An integer representing the rating from 1-5
-     * @returns {Promise<Record<string, any>>}  A movie object with a rating property appended
+     * @param {string} userId the userId for the user
+     * @param {string} movieId The tmdbId for the Movie
+     * @param {number} rating An integer representing the rating from 1-5
+     * @returns {Promise<Record<string, any>>} A movie object with a rating property
+     *          appended
      */
     // tag::add[]
-    public Map<String,Object> add(String userId, String movieId, int rating) {
-        // TODO: Convert the native integer into a Neo4j Integer
-        // TODO: Save the rating in the database
-        // TODO: Return movie details and a rating
+    public Map<String, Object> add(String userId, String movieId, int rating) {
+        // DONE: Convert the native integer into a Neo4j Integer
+        // DONE: Save the rating in the database
+        // DONE: Return movie details and a rating
 
-        var copy = new HashMap<>(pulpfiction);
-        copy.put("rating",rating);
-        return copy;
+        // Open a new session
+        try (var session = this.driver.session()) {
+
+            // Run the cypher query
+            var movie = session.writeTransaction(tx -> {
+                String query = """
+                        MATCH (u:User {userId: $userId})
+                        MATCH (m:Movie {tmdbId: $movieId})
+
+                        MERGE (u)-[r:RATED]->(m)
+                        SET r.rating = $rating, r.timestamp = timestamp()
+
+                        RETURN m { .*, rating: r.rating } AS movie
+                        """;
+                var res = tx.run(query, Values.parameters("userId", userId, "movieId", movieId, "rating", rating));
+                return res.single().get("movie").asMap();
+            });
+
+            // Return movie details with rating
+            return movie;
+        } catch (NoSuchRecordException e) {
+            throw new ValidationException("Movie or user not found to add rating",
+                    Map.of("movie", movieId, "user", userId));
+        }
+
+        // var copy = new HashMap<>(pulpfiction);
+        // copy.put("rating",rating);
+        // return copy;
     }
     // end::add[]
 }
